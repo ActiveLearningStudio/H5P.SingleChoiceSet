903f890258c6a6e6067a067b312f6dcaf2c682d7
Mahmood <mahmood.westanalytics@nxvt.com>
2021-01-22 19:09:44 +0500

    Applied check on completed statement

--- scripts/single-choice-set.js
+++ scripts/single-choice-set.js
@@ -390,7 +390,12 @@ H5P.SingleChoiceSet = (function ($, UI, Question, SingleChoice, SolutionView, Re
     self.scoreTimeout = undefined;
 
     if (!noXAPI) {
-      self.triggerXAPIScored(score, self.options.choices.length, 'completed', true, (100 * score / self.options.choices.length) >= self.options.behaviour.passPercentage);
+      if( typeof this.parent == 'undefined') { 
+        self.triggerXAPIScored(score, self.options.choices.length, 'completed', true, (100 * score / self.options.choices.length) >= self.options.behaviour.passPercentage);
+      }else{
+        self.triggerXAPIScored(score, self.options.choices.length, 'answered', true, (100 * score / self.options.choices.length) >= self.options.behaviour.passPercentage);
+      } 
+      
     }
 
     self.trigger('resize');
